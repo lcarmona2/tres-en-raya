c5060465cb6c5fd04a24035b20e70e3311d123ad
lcarmona2 <lcarmona@ayesa.com>
2026-04-14 10:10:41 +0200

    add: changes in backend

--- backend/tres-en-raya.py
+++ backend/tres-en-raya.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +16,19 @@ def check_winner(board):
 
     return None
 
+def reset():
+    return [""] * 9
+
+@app.route ('/', Methods=['GET'])
+def index():
+    build_dir = os.path.join(os.path.dirname(__file__), "frontend", "build")
+    index_path = os.path.join(build_dir, "index.html")
+
+    if os.path.exists(index_path):
+        return send_from_directory(build_dir, "index.html")
+    else:
+        return "⚙️ PQC API is running (sin frontend build)", 200
+
 @app.route("/move", methods=["POST"])
 def move():
     board = request.json["board"]
@@ -23,6 +37,13 @@ def move():
     return jsonify({
         "winner": winner
     })
+
+@app.route("/reset", methods=["POST"])
+def reset_game():
+    return jsonify({
+        "board": reset()
+    })
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
